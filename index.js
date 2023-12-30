e3c09143d8d5b11a56e42ee6f0f4ba4c2629a060
Desiah Barnett <desiahbarnett@gmail.com>
2023-12-30 16:13:21 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+   const newCatList = cats.concat([name]);
+   return newCatList
+}
+function prependCat(name) {
+    const newCatList = [(name), ...cats];
+    return newCatList
+}
+function removeLastCat() {
+    const newCatList = cats.slice();
+    newCatList.pop();
+    return newCatList;
+}
+function removeFirstCat() {
+    const newCatList = cats.slice();
+    newCatList.shift();
+    return newCatList;
+  }
+
